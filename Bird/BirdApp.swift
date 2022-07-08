103a14da09c80c128ce4847107d38986cf1859c7
Sullivan677 <decarli.sulli@gmail.com>
2022-07-08 09:20:05 +0200

    Initial Commit

---
+++ Bird/BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdApp.swift
+//  Bird
+//
+//  Created by Sullivan De carli on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bird/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bird
+//
+//  Created by Sullivan De carli on 08/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
